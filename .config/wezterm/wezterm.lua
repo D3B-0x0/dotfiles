0aa8069a9ebdaa12216f5d8f69c36841c87e9811
Kalidude69 <112244509+Kalidude69@users.noreply.github.com>
2025-02-25 10:07:45 +0530

    update wezterm conf

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,135 +1,167 @@
 -- Pull in the wezterm API
 local wezterm = require "wezterm"
-
--- This table will hold the configuration.
+local act = wezterm.action
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
--- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha"
-config.font =
-    wezterm.font("JetBrains Mono NL")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14
 config.window_decorations = "NONE"
 config.window_background_opacity = 1
 
--- tmux
 config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2500 }
 config.keys = {
-    {
-        mods = "LEADER",
-        key = "t",
-        action = wezterm.action.SpawnTab "CurrentPaneDomain",
+  {
+    mods = "LEADER",
+    key = "t",
+    action = act.SpawnTab "CurrentPaneDomain",
+  },
+  {
+    key = 'r',
+    mods = 'LEADER',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
     },
-    {
-        mods = "LEADER",
-        key = "x",
-        action = wezterm.action.CloseCurrentPane { confirm = true }
+  },
+  {
+    key = 'w',
+    mods = 'LEADER',
+    action = act.PaneSelect {
+      mode = 'MoveToNewWindow',
     },
-    {
-        mods = "LEADER",
-        key = "b",
-        action = wezterm.action.ActivateTabRelative(-1)
-    },
-    {
-        mods = "LEADER",
-        key = "n",
-        action = wezterm.action.ActivateTabRelative(1)
-    },
-    {
-        mods = "LEADER",
-        key = "v",
-        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
-    },
-    {
-        mods = "LEADER",
-        key = "-",
-        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
-    },
-    {
-        mods = "LEADER",
-        key = "h",
-        action = wezterm.action.ActivatePaneDirection "Left"
-    },
-    {
-        mods = "LEADER",
-        key = "j",
-        action = wezterm.action.ActivatePaneDirection "Down"
-    },
-    {
-        mods = "LEADER",
-        key = "k",
-        action = wezterm.action.ActivatePaneDirection "Up"
-    },
-    {
-        mods = "LEADER",
-        key = "l",
-        action = wezterm.action.ActivatePaneDirection "Right"
-    },
-    {
-        mods = "LEADER",
-        key = "LeftArrow",
-        action = wezterm.action.AdjustPaneSize { "Left", 5 }
-    },
-    {
-        mods = "LEADER",
-        key = "RightArrow",
-        action = wezterm.action.AdjustPaneSize { "Right", 5 }
-    },
-    {
-        mods = "LEADER",
-        key = "DownArrow",
-        action = wezterm.action.AdjustPaneSize { "Down", 5 }
-    },
-    {
-        mods = "LEADER",
-        key = "UpArrow",
-        action = wezterm.action.AdjustPaneSize { "Up", 5 }
-    },
+  },
+  -- Fixed new features
+  {
+    key = 'z',
+    mods = 'LEADER',
+    action = act.TogglePaneZoomState,
+  },
+  {
+    key = 'p',
+    mods = 'LEADER',
+    action = act.PaneSelect({
+      alphabet = '1234567890',
+    }),
+  },
+  {
+    key = '/',
+    mods = 'LEADER',
+    action = act.Search("CurrentSelectionOrEmptyString"),
+  },
+  {
+    key = 'f',
+    mods = 'LEADER',
+    action = act.ToggleFullScreen,
+  },
+  {
+    mods = "LEADER",
+    key = "x",
+    action = act.CloseCurrentPane { confirm = true }
+  },
+  {
+    mods = "LEADER",
+    key = "b",
+    action = act.ActivateTabRelative(-1)
+  },
+  {
+    mods = "LEADER",
+    key = "n",
+    action = act.ActivateTabRelative(1)
+  },
+  {
+    mods = "LEADER",
+    key = "v",
+    action = act.SplitHorizontal { domain = "CurrentPaneDomain" }
+  },
+  {
+    mods = "LEADER",
+    key = "-",
+    action = act.SplitVertical { domain = "CurrentPaneDomain" }
+  },
+  {
+    mods = "LEADER",
+    key = "h",
+    action = act.ActivatePaneDirection "Left"
+  },
+  {
+    mods = "LEADER",
+    key = "j",
+    action = act.ActivatePaneDirection "Down"
+  },
+  {
+    mods = "LEADER",
+    key = "k",
+    action = act.ActivatePaneDirection "Up"
+  },
+  {
+    mods = "LEADER",
+    key = "l",
+    action = act.ActivatePaneDirection "Right"
+  },
+  {
+    mods = "LEADER",
+    key = "LeftArrow",
+    action = act.AdjustPaneSize { "Left", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "RightArrow",
+    action = act.AdjustPaneSize { "Right", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "DownArrow",
+    action = act.AdjustPaneSize { "Down", 5 }
+  },
+  {
+    mods = "LEADER",
+    key = "UpArrow",
+    action = act.AdjustPaneSize { "Up", 5 }
+  },
 }
 
 for i = 0, 9 do
-    -- leader + number to activate that tab
-    table.insert(config.keys, {
-        key = tostring(i),
-        mods = "LEADER",
-        action = wezterm.action.ActivateTab(i),
-    })
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i),
+  })
 end
 
--- tab bar
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = true
 
--- tmux status
 wezterm.on("update-right-status", function(window, _)
-    local SOLID_LEFT_ARROW = ""
-    local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-    local prefix = ""
-
-    if window:leader_is_active() then
-        prefix = " " .. utf8.char(0x1f427) -- penguin 
-        SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-    end
-
-    if window:active_tab():tab_id() ~= 0 then
-        ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-    end -- arrow color based on if tab is first pane
-
-    window:set_left_status(wezterm.format {
-        { Background = { Color = "#1E1E2E" } },
-        { Text = prefix },
-        ARROW_FOREGROUND,
-        { Text = SOLID_LEFT_ARROW }
-    })
+  local SOLID_LEFT_ARROW = ""
+  local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+  local prefix = ""
+  if window:leader_is_active() then
+    prefix = " " .. utf8.char(0x1f427)
+    SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  end
+  if window:active_tab():tab_id() ~= 0 then
+    ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+  end
+  window:set_left_status(wezterm.format {
+    { Background = { Color = "#1E1E2E" } },
+    { Text = prefix },
+    ARROW_FOREGROUND,
+    { Text = SOLID_LEFT_ARROW }
+  })
 end)
 
--- and finally, return the configuration to wezterm
+config.scrollback_lines = 10000
+config.enable_scroll_bar = true
+
 return config
